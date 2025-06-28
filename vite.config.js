277ad39ef3581136498549b8fb05a364262bf8d6
Omer Geva <ogeva97@gmail.com>
2025-06-28 18:59:20 +0300

    fix: refresh issue

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,6 @@ export default defineConfig({
   plugins: [react()],
   base: '/',
   server: {
-    historyApiFallback: true,
-    allowedHosts: [
-      '81c5-2a0d-6fc0-292f-1b00-7875-a7dc-250c-9e23.ngrok-free.app'
-    ]
+    historyApiFallback: true
   }
 });
